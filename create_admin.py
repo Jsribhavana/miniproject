e8232af051487635aab7f48f60b251def6146cdf
Jsribhavana <sribhavana020@gmail.com>
2026-07-03 15:22:40 +0530

    Fixed Render deployment

--- create_admin.py
+++ create_admin.py
@@ -1,6 +1,9 @@
 import os
+import sys
+from pathlib import Path
 import django
-
+BASE_DIR = Path(__file__).resolve().parent
+sys.path.append(str(BASE_DIR))
 def create_default_superuser():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'employee_management.settings')
     django.setup()
